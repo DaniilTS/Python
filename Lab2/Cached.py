9214c2165c89a23b35a964792cbffc1f08552c7d
DaniilTS <48275635+DaniilTS@users.noreply.github.com>
2020-04-04 19:41:33 +0300

    Add files via upload

---
+++ Lab2/Cached.py
@@ -0,0 +1,30 @@
+def _cached(function):
+    def anotherFunc(arg1, arg2, arg3):
+        Sum = arg1 + arg2 + arg3
+        Mul = arg1 * arg2 * arg3
+        if len(memory) == 0:
+            memory.update({tuple([arg1, arg2, arg3]): [Sum, Mul]})
+            print("Sum:", Sum)
+            function(arg1, arg2, arg3)
+
+        elif tuple([arg1, arg2, arg3]) not in memory:
+            memory.update({tuple([arg1, arg2, arg3]): [Sum, Mul]})
+            print("Sum:", Sum)
+            function(arg1, arg2, arg3)
+
+        else:
+            print("Sum and Mul:", memory[tuple([arg1, arg2, arg3])])
+    return anotherFunc
+
+
+@_cached
+def print_sum_then_mul(arg1, arg2, arg3):
+    print("Mul:", arg1 * arg2 * arg3)
+
+
+i = 0
+memory = {}
+N = int(input())
+while i < N:
+    num1, num2, num3 = map(int, input().split())
+    print_sum_then_mul(num1, num2, num3)
