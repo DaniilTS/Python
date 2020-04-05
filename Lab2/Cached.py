4223f916b2ccee90c45872955c51cf13f74c92bc
DaniilTS <48275635+DaniilTS@users.noreply.github.com>
2020-04-05 03:03:54 +0300

    Update Cached.py

--- Lab2/Cached.py
+++ Lab2/Cached.py
@@ -1,21 +1,15 @@
 def _cached(function):
     def anotherFunc(arg1, arg2, arg3):
-        Sum = arg1 + arg2 + arg3
-        Mul = arg1 * arg2 * arg3
+        Sum, Mul = arg1 + arg2 + arg3, arg1 * arg2 * arg3
         key = tuple([arg1, arg2, arg3])
 
-        if len(memory) == 0:
-            memory.update({key: [Sum, Mul]})
-            print("Sum:", Sum)
-            function(arg1, arg2, arg3)
-
-        elif key not in memory:
-            memory.update({key: [Sum, Mul]})
+        if key not in Dict:
+            Dict.update({key: [Sum, Mul]})
             print("Sum:", Sum)
             function(arg1, arg2, arg3)
 
         else:
-            print("Sum and Mul:", memory[key])
+            print("Sum and Mul:", Dict[key])
     return anotherFunc
 
 
@@ -24,9 +18,7 @@ def print_sum_then_mul(arg1, arg2, arg3):
     print("Mul:", arg1 * arg2 * arg3)
 
 
-i = 0
-memory = {}
-N = int(input())
-while i < N:
+Dict = {}
+for i in range(int(input())):
     num1, num2, num3 = map(int, input().split())
     print_sum_then_mul(num1, num2, num3)
